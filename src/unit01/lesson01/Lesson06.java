a59c2e595e75270221c750d3b863f35585e7674c
Anderson <anderson.java.dev@gmail.com>
2022-06-29 18:41:51 -0300

    Operadores de atribuição

---
+++ src/unit01/lesson01/Lesson06.java
@@ -0,0 +1,30 @@
+package unit01.lesson01;
+
+import util.Util;
+
+public class Lesson06 {
+    public static void main(String[] args) {
+        Util.printTitle("Operadores de Atribuição");
+
+        // +=
+        int valueA = 20;
+        valueA += 10; // valueA = valueA + 10;
+        System.out.println("Resultado da soma utilizando\noperador de atribuição (+=): " + valueA + "\n");
+
+        // -=
+        int valueB = 20;
+        valueB -= 10; // valueB = valueB - 10;
+        System.out.println("Resultado da subtração utilizando\noperador de atribuição (-=): " + valueB + "\n");
+
+        // *=
+        int valueC = 20;
+        valueC *= 10; // valueB = valueB * 10;
+        System.out.println("Resultado da multiplicação utilizando\noperador de atribuição (*=): " + valueC + "\n");
+
+        // /=
+        int valueD = 20;
+        valueD /= 10; // valueB = valueB / 10;
+        System.out.println("Resultado da divisão utilizando\noperador de atribuição (/=): " + valueD + "\n");
+
+    }
+}
