6f3c2937e0a8d3fe33edb1838228d08557a1f77e
Anderson <anderson.java.dev@gmail.com>
2022-06-30 00:07:17 -0300

    Estrutura de Repetição - Break

---
+++ src/unit02/Lesson12.java
@@ -0,0 +1,35 @@
+package unit02;
+
+import util.Util;
+
+public class Lesson12 {
+    public static void main(String[] args) {
+
+        Util.printTitle("Estrutura de Repetição - Break");
+
+        // Parar a estrutura de repetição com o Break
+        int valueMax = 10;
+
+        for (int i = 0; i < valueMax; i++) {
+
+            if(i > 5){
+                break;
+            }
+            System.out.println("i : " + i);
+        }
+        System.out.println("Fim do laço\n");
+
+        // Dado o valor de um carro, descubra em quantas prestações ele pode ser parcelado
+        // condição -> valor da parcela deve ser superior a R$ 2.000,00
+        double valorCarro = 30000;
+        System.out.println("Condição de pagamento do Carro de R$ " + valorCarro);
+        for (int parcela = 1; parcela <= valorCarro; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 2000){
+                break;
+            }
+            System.out.println("Prestações : " + parcela + " - Valor da parcela R$ " + valorParcela);
+        }
+
+    }
+}
