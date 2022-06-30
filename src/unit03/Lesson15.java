6603b89764be1779065bfe0bf24ca0c2945f9fb9
Anderson <anderson.java.dev@gmail.com>
2022-06-30 01:02:21 -0300

    Array - atributo length

---
+++ src/unit03/Lesson15.java
@@ -0,0 +1,20 @@
+package unit03;
+
+import util.Util;
+
+public class Lesson15 {
+    public static void main(String[] args) {
+
+        Util.printTitle("Array - atributo length");
+
+        Integer[] age = new Integer[4];
+        age[0] = 19;
+        age[1] = 25;
+        age[2] = 36;
+        age[3] = 49;
+
+        for (int i = 0; i < age.length; i++) {
+            System.out.println("Posição do array : " + i + " contém o valor " + age[i]);
+        }
+    }
+}
