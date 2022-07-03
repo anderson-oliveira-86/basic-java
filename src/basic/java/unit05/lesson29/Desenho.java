2fb915ae793de192bb3eb9db2f04c90962868bef
Anderson <anderson.java.dev@gmail.com>
2022-07-03 14:22:21 -0300

    Orientação Objetos - Sobrescrita do método toString

---
+++ src/basic/java/unit05/lesson29/Desenho.java
@@ -0,0 +1,24 @@
+package basic.java.unit05.lesson29;
+
+public class Desenho {
+    private String nome;
+
+    public Desenho(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Desenho{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
---
+++ src/basic/java/unit05/lesson29/Lesson29.java
@@ -0,0 +1,12 @@
+package basic.java.unit05.lesson29;
+
+import basic.java.util.Util;
+
+public class Lesson29 {
+    public static void main(String[] args) {
+        Util.printTitle("Orientação Objetos - Sobrescrita do método toString");
+
+        Desenho desenho = new Desenho("Pica-Pau");
+        System.out.println(desenho);
+    }
+}
