f5ab4bf2ae8e675e1cea9282b1789c9d04cbdd59
Anderson <anderson.java.dev@gmail.com>
2022-07-03 14:36:05 -0300

    Orientação Objetos - Modificador final pt 01 - Tipo primitivo

---
+++ src/basic/java/unit05/lesson30/Lesson30.java
@@ -0,0 +1,11 @@
+package basic.java.unit05.lesson30;
+
+import basic.java.util.Util;
+
+public class Lesson30 {
+    public static void main(String[] args) {
+        Util.printTitle("Orientação Objetos - Modificador final pt 01 - Tipo primitivo");
+
+        System.out.println(Veiculo.VELOCIDADE_LIMITE);
+    }
+}
---
+++ src/basic/java/unit05/lesson30/Veiculo.java
@@ -0,0 +1,14 @@
+package basic.java.unit05.lesson30;
+
+public class Veiculo {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
