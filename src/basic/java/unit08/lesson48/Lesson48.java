8edccff22ab7a985cec281a429da3ffd68b0580a
Anderson <anderson.java.dev@gmail.com>
2022-07-04 19:44:28 -0300

    Classes Utilitárias - Wrappers pt 01

--- src/basic/java/unit08/lesson48/Lesson48.java
+++ src/basic/java/unit08/lesson48/Lesson48.java
@@ -1,7 +1,32 @@
 package basic.java.unit08.lesson48;
 
+import basic.java.util.Util;
+
 public class Lesson48 {
     public static void main(String[] args) {
+
+        Util.printTitle("Classes Utilitárias - Wrappers pt 01");
+
+        byte bytePrimitivo = 1;
+        short shortPrimitivo = 1;
+        int intPrimitivo = 1;
+        long longPrimitivo = 1L;
+        float floatPrimitivo = 1;
+        double doublePrimitivo = 1;
+        char charPrimitivo = 'A';
+        boolean Primitivo = false;
+
+        // Classes Wrappers de tipos primitivos
+        Byte byteWrapper = 1;
+        Short shortWrapper = 1;
+        Integer intWrapper = 1;
+        Long longWrapper = 1L;
+        Float floatWrapper = 1F;
+        Double doubleWrapper = 1D;
+        Character charWrapper = 'A';
+        Boolean Wrapper = false;
+
+        
 
     }
 }
