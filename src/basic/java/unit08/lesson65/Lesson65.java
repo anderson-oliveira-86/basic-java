79ead26e55a26a77d9f8057b50be3bde12d9eefb
Anderson <anderson.java.dev@gmail.com>
2022-07-06 16:38:06 -0300

    Classes Utilitárias - Period

--- src/basic/java/unit08/lesson65/Lesson65.java
+++ src/basic/java/unit08/lesson65/Lesson65.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 public class Lesson65 {
     public static void main(String[] args) {
@@ -23,9 +24,15 @@ public class Lesson65 {
         Duration d1 = Duration.between(now, nowAfterTwoYears);
         Duration d2 = Duration.between(timeNow, timeMinusSevenHours);
         Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofDays(20);
+        Duration d5 = Duration.ofMinutes(3);
+        Duration d6 = Duration.of(3, ChronoUnit.HOURS);
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+        System.out.println(d6);
 
     }
 }
