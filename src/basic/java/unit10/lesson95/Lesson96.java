0e2aa8ae7d552964b0b45d42feefac2a8e17b47b
Anderson <anderson.java.dev@gmail.com>
2022-07-09 16:38:03 -0300

    Classes Utilitárias - NIO pt 10 - DirectoryStream

---
+++ src/basic/java/unit10/lesson95/Lesson96.java
@@ -0,0 +1,29 @@
+package basic.java.unit10.lesson95;
+
+import basic.java.util.Util;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Lesson96 {
+    public static void main(String[] args) {
+
+        Util.printTitle("Classes Utilitárias - NIO pt 10 - DirectoryStream");
+
+        Path dir = Paths.get(".");
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for (Path path :
+                    stream) {
+                System.out.println(path.getFileName());
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
