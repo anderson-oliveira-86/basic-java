ad3c7b2a38c68b511baf8f1064192bbd39faf55d
Anderson <anderson.java.dev@gmail.com>
2022-07-09 21:03:27 -0300

    Classes Utilitárias - NIO pt 15 - ZipOutputStream

---
+++ src/basic/java/unit10/lesson99/Lesson99.java
@@ -0,0 +1,38 @@
+package basic.java.unit10.lesson99;
+
+import basic.java.util.Util;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Lesson99 {
+    public static void main(String[] args) {
+        Util.printTitle("Classes Utilitárias - NIO pt 15 - ZipOutputStream");
+
+        Path arquivoZip = Paths.get("pasta/arquivo.zip");
+        Path arquivosParaZipar = Paths.get("pasta/subpasta1/subsubpasta1");
+        zip(arquivoZip ,arquivosParaZipar);
+    }
+
+    private static void zip(Path arquivoZip , Path arquivosParaZipar){
+        try(ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar)){
+
+            for (Path file :
+                    directoryStream) {
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zipStream.putNextEntry(zipEntry);
+                Files.copy(file , zipStream);
+                zipStream.closeEntry();
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
